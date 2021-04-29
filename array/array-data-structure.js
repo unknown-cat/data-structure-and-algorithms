335925280231d4c10c01379a37c105a73cca217b
unknown-cat <twilightcat73@yahoo.com>
2021-04-29 13:55:04 +0300

    Update(array): array-data-structure.js
    delete item at the index of array

--- array/array-data-structure.js
+++ array/array-data-structure.js
@@ -12,7 +12,7 @@ class MyArray {
     this.data[this.length] = item;
     this.length++;
     return this.length;
-  }//O(1)
+  } //O(1)
 
   pop() {
     const lastItem = this.data[this.length - 1];
@@ -21,6 +21,18 @@ class MyArray {
     return lastItem;
   } //O(1)
 
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItem(index)
+  } //O(1 + n) = O(n)
+
+  shiftItem(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  } //O(n)
 }
 
 const newArray = new MyArray();
