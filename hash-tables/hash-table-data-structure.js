a0075b2f697658051c2c2ca707eb3a24193bcdba
unknown-cat <twilightcat73@yahoo.com>
2021-04-30 20:19:42 +0300

    Update(hash-tables): hash-tables-data-structure.js
    create own hashing function

--- hash-tables/hash-table-data-structure.js
+++ hash-tables/hash-table-data-structure.js
@@ -2,6 +2,15 @@ class HashTable {
   constructor(size) {
     this.data = new Array(size)
   }
+
+  _hash(key) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  } //really fast we considering it like O(n)
 }
 
 const myHashTable = new HashTable(20);
