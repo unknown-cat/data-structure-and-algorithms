a8b00d5e834b536922e05c144be15532ce4b8429
unknown-cat <twilightcat73@yahoo.com>
2021-05-04 18:39:55 +0300

    Update(linked-list): singly-linked-list-data-structure.js
    create append function which appends a new node at the end

--- linked-list/singly-linked-list-data-structure.js
+++ linked-list/singly-linked-list-data-structure.js
@@ -8,4 +8,13 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
 }
