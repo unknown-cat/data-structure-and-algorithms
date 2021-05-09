38f5dc8a1e6a2b0bff6bc94710f2a74c4d0a5f25
unknown-cat <twilightcat73@yahoo.com>
2021-05-09 16:25:02 +0300

    Update(linked-list): doubly-linked-list-data-structure.js
    create class newNode and functions append and prepend

--- linked-list/doubly-linked-list-data-structure.js
+++ linked-list/doubly-linked-list-data-structure.js
@@ -1,3 +1,11 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
 class DoublyLinkedList {
   constructor(value) {
     this.head = {
@@ -10,6 +18,23 @@ class DoublyLinkedList {
     this.length = 1;
   }
 
+  append(value) {
+    let newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 const myDoublyLinkedList = new DoublyLinkedList(10);
