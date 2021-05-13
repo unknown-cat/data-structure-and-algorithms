828c3310a4688a8eb70796514cd6bd531150f58e
unknown-cat <twilightcat73@yahoo.com>
2021-05-13 15:27:06 +0300

    Update(singly-linked-list): add reverse function
    reverse linked list

---
+++ linked-list/reverse-single-linked-list-data-structure.js
@@ -0,0 +1,120 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value)
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    return this.printList()
+  }
+
+  remove(index) {
+
+    if (index <= 0) {
+      return this.removeFirst()
+    }
+
+    if (index >= this.length) {
+      return this.removeLast()
+    }
+
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList()
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++
+    }
+
+    return currentNode
+  }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head
+    }
+
+    let first = this.head;
+    this.tail = first;
+    let second = first.next;
+
+    while (second) {
+      let temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+}
+
+const myLinkedList = new LinkedList(22);
+myLinkedList.append(2);
+myLinkedList.append(3);
+myLinkedList.append(55);
+console.log(myLinkedList.printList());
+
+console.log(myLinkedList.reverse());
+
+
